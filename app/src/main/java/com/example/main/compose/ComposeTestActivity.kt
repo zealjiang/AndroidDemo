c6e7d2abf946bbcb030e5c09e318dbd74f24e716
lizhijiang <wb_lizhijiang@meituan.com>
2024-02-01 13:23:21 +0800

    feat(compose)添加compose相关依赖

---
+++ app/src/main/java/com/example/main/compose/ComposeTestActivity.kt
@@ -0,0 +1,27 @@
+package com.example.main.compose
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class ComposeTestActivity : ComponentActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{}
+    }
+
+
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name")
+}
