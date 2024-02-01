d1d6fba0aa6f7b6ce39b683528fb06465f998a9d
lizhijiang <wb_lizhijiang@meituan.com>
2024-02-01 14:17:10 +0800

    feat(compose)添加compose相关依赖

--- app/src/main/java/com/example/main/compose/ComposeTestActivity.kt
+++ app/src/main/java/com/example/main/compose/ComposeTestActivity.kt
@@ -20,10 +20,9 @@ class ComposeTestActivity : ComponentActivity(){
         }
     }
 
-
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name")
+    }
 }
 
-@Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name")
-}
